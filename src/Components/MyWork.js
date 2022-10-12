ba16e88a35700b046cb263670b620aa966147144
Halleluyah <adesuyihalleluyah@gmail.com>
2022-10-12 23:40:28 +0100

    Update App.css

--- src/Components/MyWork.js
+++ src/Components/MyWork.js
@@ -3,28 +3,28 @@ import githubLogo from "../img/github.png"
 
 export default function MyWork(props) {
   return (
-    <div className="each-project">
+    <section className="each-project">
         <img src={props.src} alt={props.alt} className="project-img" />
-        <h4>{props.name}</h4>
+        <h4 className="project-name">{props.name}</h4>
 
-        <p className="tech-used">
+        <section className="tech-used">
             <span className="tech">{props.tech1}</span>
             <span className="tech">{props.tech2}</span>
             <span className="tech">{props.tech3}</span>
             <span className="tech">{props.tech4}</span>
             <span className="tech">{props.tech5}</span>
             <span className="tech">{props.tech6}</span>
-        </p>
+        </section>
         
         <p className="description">{props.description}</p>
 
-        <div className="liveDemo-and-github">
+        <section className="liveDemo-and-github">
             <p className="live-demo"><a href={props.liveDemo} target="_blank">Live Demo</a></p>
             <div className="github-flex">
                 <p><a href={props.github} target="_blank">Github</a></p>
                 <img src={githubLogo} alt="Github Logo" className="github-logo" />
             </div>
-        </div>
-    </div>
+        </section>
+    </section>
   )
 }
