46fb044b756b7bf5b496523341239ae80eaf16fb
Halleluyah <adesuyihalleluyah@gmail.com>
2022-10-13 17:05:14 +0100

    Update

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,20 +1,23 @@
-import React from 'react'
+import React from "react"
+import instagram from "../img/instagram.png"
+import linkedin from "../img/linkedin.png"
+import githubLogo from "../img/github-myWork.png"
 
 export default function Footer() {
   return (
     <footer>
         <span>Copyright &copy; Halleluyah Adesuyi 2022</span>
         <section className="contact">
-          <a href="" target="_blank">
-            <img src="" alt="Instagram Link" className="contact-instagram" />
+          <a href="https://www.instagram.com/ifeoluwa_hallel/" target="_blank">
+            <img src={instagram} alt="Instagram Link" className="contact-instagram" />
           </a>
 
-          <a href="" target="_blank">
-            <img src="" alt="Linkedin Link" className="contact-linkedin" />
+          <a href="https://www.linkedin.com/in/halleluyahadesuyi/" target="_blank">
+            <img src={linkedin} alt="Linkedin Link" className="contact-linkedin" />
           </a>
 
-          <a href="" target="_blank" className="contact-github">
-            <img src="" alt="Github Link" />
+          <a href="https://github.com/halleluyahadesuyi" target="_blank" alt="Github Link" className="contact-github">
+            <img src={githubLogo} alt="Github Link" />
           </a>
         </section>
     </footer>
