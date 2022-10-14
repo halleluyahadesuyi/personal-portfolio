17f343aa1685d2c296e7b61135c7b67c41e9f0f0
Halleluyah <adesuyihalleluyah@gmail.com>
2022-10-14 03:33:03 +0100

    Update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
+import React from "react"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+
 import Nav from "./Components/Nav"
 import Profile from "./Components/Profile"
 import AboutMe from "./Components/AboutMe"
 import Certifications from "./Components/Certifications"
-import Footer from "./Components/Footer"
-import decorativeUnderline from "./img/line-star-line.png"
 import MyWork from "./Components/MyWork"
+import Footer from "./Components/Footer"
+
+import decorativeUnderline from "./img/line-star-line.png"
 import imageGallery from "./img/image-gallery.png"
 import chromeExtension from "./img/chrome-extension.PNG"
 import businessPage from "./img/business-page.PNG"
@@ -15,84 +19,88 @@ import "./App.css"
 
 function App() {
   return (
-    <div className="App">
-      <Nav />
+    <Router>
+      <div className="App">
+        <Nav />
 
-      <Profile />
+        <Profile />
 
-      <AboutMe />
+        <Routes>
+          <Route path="/about" element={<AboutMe to="/" />} />
+        </Routes>
 
-      <Certifications />
+        <Certifications />
 
-      <section className="my-work">
-        <h2>MY WORK</h2>
-        <span className="italicize">..some of my projects</span>
-        <img
-          src={decorativeUnderline}
-          alt="Decorative Underline"
-          className="underline"
-        />
-
-        <div className="projects">
-          <MyWork
-            src={imageGallery}
-            alt="responsive image gallery app"
-            projectLink="https://responsive-image-gallery-using-unsplash-api.netlify.app/"
-            name="Responsive Image Gallery App"
-            tech1="React" tech2="Unsplash API" tech3="JavaScript"
-            tech4="CSS" tech5="HTML"
-            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-            liveDemo="https://responsive-image-gallery-using-unsplash-api.netlify.app/"
-            github="https://github.com/halleluyahadesuyi/responsive-image-gallery-using-unsplash-api"
+        <section className="my-work">
+          <h2>MY WORK</h2>
+          <span className="italicize">..some of my projects</span>
+          <img
+            src={decorativeUnderline}
+            alt="Decorative Underline"
+            className="underline"
           />
-          <MyWork
-            src={chromeExtension}
-            alt="chrome extension"
-            projectLink="https://chrome-dashboard-extension-using-coingecko-api.netlify.app/"
-            name="Chrome Extension"
-            tech1="JavaScript" tech2="GeoLocation Web API" tech3="CoinGecko API" tech4="OpenWeatherMap API" tech5="CSS" tech6="HTML"
-            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-            liveDemo="https://chrome-dashboard-extension-using-coingecko-api.netlify.app/"
-            github="https://github.com/halleluyahadesuyi/chrome-dashboard-extension-using-coingecko-api"
-          />
-          <MyWork
-            src={businessPage}
-            alt="business landing page"
-            projectLink="https://business-landing-page-plus-subscription.netlify.app/"
-            name="A Business Landing Page"
-            tech1="React" tech2="Unsplash API" tech3="JavaScript"
-            tech4="HTML" tech5="CSS"
-            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-            liveDemo="https://business-landing-page-plus-subscription.netlify.app/"
-            github="https://github.com/halleluyahadesuyi/business-landing-page"
-          />
-          <MyWork
-            src={survivorGame}
-            alt="survivor game"
-            projectLink="https://thrilling-survivor-game-wizard-and-monsters.netlify.app/"
-            name="A Thrilling Survivor Game"
-            tech1="React" tech2="Unsplash API" tech3="JavaScript"
-            tech4="HTML" tech5="CSS"
-            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-            liveDemo="https://thrilling-survivor-game-wizard-and-monsters.netlify.app/"
-            github="https://github.com/halleluyahadesuyi/thrilling-survivor-game-wizard-and-monsters"
-          />
-          <MyWork
-            src={comingSoonest}
-            alt="ongoing project"
-            name="React E-Commerce Website ---"
-            soonest=" coming soon"
-            tech1="React" tech2="GraphQL" tech3="JavaScript"
-            tech4="HTML" tech5="CSS"
-            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-            liveDemo="#"
-            github="#"
-          />
-        </div>
-      </section>
 
-      <Footer />
-    </div>
+          <div className="projects">
+            <MyWork
+              src={imageGallery}
+              alt="responsive image gallery app"
+              projectLink="https://responsive-image-gallery-using-unsplash-api.netlify.app/"
+              name="Responsive Image Gallery App"
+              tech1="React" tech2="Unsplash API" tech3="JavaScript"
+              tech4="CSS" tech5="HTML"
+              description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+              liveDemo="https://responsive-image-gallery-using-unsplash-api.netlify.app/"
+              github="https://github.com/halleluyahadesuyi/responsive-image-gallery-using-unsplash-api"
+            />
+            <MyWork
+              src={chromeExtension}
+              alt="chrome extension"
+              projectLink="https://chrome-dashboard-extension-using-coingecko-api.netlify.app/"
+              name="Chrome Extension"
+              tech1="JavaScript" tech2="GeoLocation Web API" tech3="CoinGecko API" tech4="OpenWeatherMap API" tech5="CSS" tech6="HTML"
+              description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+              liveDemo="https://chrome-dashboard-extension-using-coingecko-api.netlify.app/"
+              github="https://github.com/halleluyahadesuyi/chrome-dashboard-extension-using-coingecko-api"
+            />
+            <MyWork
+              src={businessPage}
+              alt="business landing page"
+              projectLink="https://business-landing-page-plus-subscription.netlify.app/"
+              name="A Business Landing Page"
+              tech1="React" tech2="Unsplash API" tech3="JavaScript"
+              tech4="HTML" tech5="CSS"
+              description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+              liveDemo="https://business-landing-page-plus-subscription.netlify.app/"
+              github="https://github.com/halleluyahadesuyi/business-landing-page"
+            />
+            <MyWork
+              src={survivorGame}
+              alt="survivor game"
+              projectLink="https://thrilling-survivor-game-wizard-and-monsters.netlify.app/"
+              name="A Thrilling Survivor Game"
+              tech1="React" tech2="Unsplash API" tech3="JavaScript"
+              tech4="HTML" tech5="CSS"
+              description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+              liveDemo="https://thrilling-survivor-game-wizard-and-monsters.netlify.app/"
+              github="https://github.com/halleluyahadesuyi/thrilling-survivor-game-wizard-and-monsters"
+            />
+            <MyWork
+              src={comingSoonest}
+              alt="ongoing project"
+              name="React E-Commerce Website ---"
+              soonest=" coming soon"
+              tech1="React" tech2="GraphQL" tech3="JavaScript"
+              tech4="HTML" tech5="CSS"
+              description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+              liveDemo="#"
+              github="#"
+            />
+          </div>
+        </section>
+
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,4 +1,5 @@
-import React, { useState}  from "react"
+import React from "react"
+import { Link } from "react-router-dom"
 import logo from "../img/logo.png"
 import emailIcon from "../img/email-icon.png"
 
@@ -8,36 +9,37 @@ export default function Nav() {
         navbarLinks.classList.toggle("navbar-links-active")
     }
 
-  return (
-    <nav className="navbar">
-        <img 
-            src={logo}
-            alt="brand logo"
-            className="logo"
-        />
-
-        <a href="mailto:adesuyihalleluyah@gmail.com" target="_blank" className="get-in-touch-navbar">
-            <h5>GET IN TOUCH!</h5>
-            <img 
-                src={emailIcon}
-                alt="Email Icon"
-                className="email-icon"
+    return (
+        <nav className="navbar">
+            <img
+                src={logo}
+                alt="brand logo"
+                className="logo"
             />
-        </a>
 
-        <div className="navbar-links">
-            <ul>
-                <li><a href="#">ABOUT</a></li>
-                <li><a href="#">PORTFOLIO</a></li>
-                <li><a href="#">CONTACT</a></li>
-            </ul>
-        </div>
+            <a href="mailto:adesuyihalleluyah@gmail.com" target="_blank" className="get-in-touch-navbar">
+                <h5>GET IN TOUCH!</h5>
+                <img
+                    src={emailIcon}
+                    alt="Email Icon"
+                    className="email-icon"
+                />
+            </a>
 
-        <div className="hamburger-toggle" onClick={hamburgerToggle}>
-            <span className="bar"></span>
-            <span className="bar"></span>
-            <span className="bar"></span>
-        </div>
-    </nav>
-  )
+            <div className="navbar-links">
+                <ul>
+                    <li><Link to="/">-</Link></li>
+                    <li><Link to="/about">ABOUT</Link></li>
+                    <li><a href="#">PORTFOLIO</a></li>
+                    <li><a href="#">CONTACT</a></li>
+                </ul>
+            </div>
+
+            <div className="hamburger-toggle" onClick={hamburgerToggle}>
+                <span className="bar"></span>
+                <span className="bar"></span>
+                <span className="bar"></span>
+            </div>
+        </nav>
+    )
 }
