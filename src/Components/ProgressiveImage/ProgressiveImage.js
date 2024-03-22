bf4fd5e04470687299ecde3f0de7f5a175ac4b6b
Halleluyah Adesuyi <adesuyihalleluyah@gmail.com>
2024-03-22 13:26:38 +0100

    feat: Incorporate progressive loading of all images

---
+++ src/Components/ProgressiveImage/ProgressiveImage.js
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from "react";
+
+const ProgressiveImage = ({ src, placeholderSrc, alt, className }) => {
+  const [imgSrc, setImgSrc] = useState(placeholderSrc || src);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const img = new Image();
+    img.src = src;
+    img.onload = () => {
+      setImgSrc(src);
+      setLoading(false);
+    };
+  }, [src]);
+
+  return (
+    <img
+      src={loading ? placeholderSrc : imgSrc}
+      alt={alt}
+      className={`${className} ${loading ? "loading" : "loaded"}`}
+    />
+  );
+};
+
+export default ProgressiveImage;
